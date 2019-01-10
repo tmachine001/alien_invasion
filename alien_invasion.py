e510f059affcd96cc1c62aab6b26bb5ec2f1ebc0
Ty Christian <tchristian@ryeneck.k12.ny.us>
2019-01-10 14:50:56 -0500

    Bullet projection is functional!

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,9 @@
 import pygame
-from settings import Settings
-from ship import Ship
 import game_functions as gf
 
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
 
 def run_game():
     #Initialize game, settings, and screen object.
@@ -14,11 +15,14 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets
+    bullets = Group()
 
     # Start the main loop for the game
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         
 run_game()
