2ded12dde7f8b41d26e5c941e7211debf8a4ed29
tmachine001 <tmachine0001@gmail.com>
2019-01-10 11:17:31 -0500

    Let and Right functionality, 1.5 pixel movement speed

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship():
     def __init__(self, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,25 @@ class Ship():
         # Start each new shp at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value tfor the ship's center.
+        self.center = float(set.rect.centerx)
+
+        # Movement Flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's center value, not the rect.
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
+
 
     def blitme(self):
         """Draw the ship at its current location."""
