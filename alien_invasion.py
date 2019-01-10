255aa488a3160ad20104290e93e8e2e19ab91b51
tmachine001 <tmachine0001@gmail.com>
2019-01-10 09:28:40 -0500

    Basic settings, ship and gameboard build;

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+import sys, pygame
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    #Initialize game, settings, and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
+
+    # Start the main loop for the game
+    while True:
+
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the scren during each pass through loop.
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+
+run_game()
